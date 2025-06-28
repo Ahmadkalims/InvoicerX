565c28a78372194b2c215f5c0879e467c9fa346b
Ahmadkalims <ahmadkalims@yahoo.com>
2025-06-28 23:54:04 +0530

    reduced index.html

--- src/components/NavButton.jsx
+++ src/components/NavButton.jsx
@@ -3,11 +3,40 @@ import React from 'react';
 const NavButton = ({ icon, label }) => {
   return (
     <div
-      className="flex items-center gap-2 px-3 py-2 rounded-full bg-white/10 backdrop-blur-md text-white transition-all duration-300 ease-in-out cursor-pointer overflow-hidden w-[44px] hover:w-auto group"
+      className="
+        flex items-center 
+        gap-[1px]
+        px-3 py-3
+        rounded-full 
+
+        text-white text-sm
+        cursor-pointer
+
+        overflow-hidden
+        max-w-[45px]
+        hover:max-w-[200px]
+
+        transition-all duration-300 ease-in-out
+        group
+
+        bg-transparent
+        hover:bg-white/10 hover:backdrop-blur-md
+        active:bg-white/10 active:backdrop-blur-md
+      "
     >
       <img src={icon} alt={label} className="w-5 h-5 shrink-0" />
+      
       <span
-        className="whitespace-nowrap opacity-0 group-hover:opacity-100 transition-opacity duration-300 ml-2"
+        className="
+          whitespace-nowrap
+          opacity-0 invisible
+          ml-2
+          
+          transition-all duration-300 ease-in
+          
+          group-hover:opacity-100 
+          group-hover:visible
+        "
       >
         {label}
       </span>
