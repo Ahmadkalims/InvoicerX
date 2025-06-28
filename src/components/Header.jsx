0b7e0cfa94c0c84f4926297cfba8df6c7e40f0fb
Ahmadkalims <ahmadkalims@yahoo.com>
2025-06-29 00:20:24 +0530

    structure improvement

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,14 +6,17 @@ const Header = () => {
 
     
     <header className="fixed top-5 left-1/2 -translate-x-1/2 bg-white/10 backdrop-blur-md p-2 rounded-full flex gap-2 z-50">
+      
       <NavButton
         icon="https://img.icons8.com/?size=24&id=1iF9PyJ2Thzo&format=png&color=FFFFFF"
         label="Home"
       />
+
       <NavButton
         icon="https://img.icons8.com/?size=24&id=M1HUQOE9lLoG&format=png&color=FFFFFF"
         label="Experience"
       />
+
       <NavButton
         icon="https://img.icons8.com/?size=24&id=79387&format=png&color=FFFFFF"
         label="Education"
@@ -23,6 +26,7 @@ const Header = () => {
         icon="https://img.icons8.com/?size=24&id=6H65NWsqha0Z&format=png&color=FFFFFF"
         label="Contact"
       />
+      
     </header>
   );
 };
--- src/components/NavButton.jsx
+++ src/components/NavButton.jsx
@@ -3,8 +3,9 @@ import React from 'react';
 const NavButton = ({ icon, label }) => {
   return (
     <div
+      style={{ fontFamily: "'Montserrat', sans-serif" }}
       className="
-        flex items-center 
+        flex items-center
         gap-[1px]
         px-3 py-3
         rounded-full 
@@ -21,26 +22,26 @@ const NavButton = ({ icon, label }) => {
 
         bg-transparent
         hover:bg-white/10 hover:backdrop-blur-md
-        active:bg-white/10 active:backdrop-blur-md
-      "
-      style={{ fontFamily: "'Montserrat', sans-serif" }}
-    >
-      <img src={icon} alt={label} className="w-5 h-5 shrink-0" />
+        active:bg-white/10 active:backdrop-blur-md">
+
+
+        <img src={icon} alt={label} className="w-5 h-5 shrink-0" />
       
-      <span
+        <span
         className="
           whitespace-nowrap
           opacity-0 invisible
-          ml-2
+          ml-1
           
           transition-all duration-300 ease-in
           
           group-hover:opacity-100 
-          group-hover:visible
-        "
-      >
+          group-hover:visible">
         {label}
+        
       </span>
+
+
     </div>
   );
 };
