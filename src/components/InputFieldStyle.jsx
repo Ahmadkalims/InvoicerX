cae5027ae16b5a1bbd38807d5060a283462340fb
Ahmadkalims <ahmadkalims@yahoo.com>
2025-06-29 21:12:51 +0530

    Updated forms font

--- src/components/InputFieldStyle.jsx
+++ src/components/InputFieldStyle.jsx
@@ -2,15 +2,15 @@ import React from "react";
 
 const InputFieldStyle = ({ label, placeholder, type = 'text' }) => {
   return (
-    <div className="mb-4"
-    style={{ fontFamily: "'Montserrat-light', sans-serif", fontSize: "px" }}>
-      <label className="block text-[#4d4d4d] text-xl font-light mb-2 ml-3">
+    <div className="mb-4">
+      <label style={{ fontFamily: "'Montserrat-light', sans-serif", fontSize: "px" }}
+      className="block text-[#4d4d4d] mb-2 ml-3">
         {label}
       </label>
       <input
         type={type}
         placeholder={placeholder}
-        className="w-[252px] bg-[#e6e6e6] text-gray-700 text-[16px] font-light px-6 py-4 rounded-[16px] focus:outline-none shadow-inner"
+        className="w-[252px] bg-[#e6e6e6] text-[#4d4d4d] px-6 py-4 rounded-[16px] focus:outline-none shadow-inner"
       />
     </div>
   );
